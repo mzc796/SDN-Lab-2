b01a60ac154666907c931df15a388b8d77f17d66
Mingming Chen <55955582+mzc796@users.noreply.github.com>
2026-02-15 15:01:06 -0600

    Update read_operational_table.sh

--- read_operational_table.sh
+++ read_operational_table.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "reading config data store of switch $1"
+echo "reading operational data store of switch $1"
 
 curl -s -u admin:admin -X GET "http://localhost:8181/rests/data/opendaylight-inventory:nodes/node=$1/flow-node-inventory:table=0?content=nonconfig" -H "Content-Type: application/json" -o data/$1_config_table.json
